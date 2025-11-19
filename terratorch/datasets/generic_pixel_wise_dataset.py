dc9de7efced4e202ccc4700089380c5d1ac7235c
Viet Duc Kieu <viet.duc.kieu@web.de>
2025-11-19 13:21:01 +0100

    fix: add missing logger

--- terratorch/datasets/generic_pixel_wise_dataset.py
+++ terratorch/datasets/generic_pixel_wise_dataset.py
@@ -706,6 +706,8 @@ class GenericNonGeoPixelwiseDataset_Custom(GenericPixelWiseDataset):
                 )
 
     def _load_file(self, path, nan_replace: int | float | None = None) -> xr.DataArray:
+        logger = logging.getLogger("terratorch")
+
         # Target order of Sentinel-2 bands expected by the model
         BAND_ORDER = ["B01","B02","B03","B04","B05","B06","B07","B08","B8A","B09","B11","B12"]
 
